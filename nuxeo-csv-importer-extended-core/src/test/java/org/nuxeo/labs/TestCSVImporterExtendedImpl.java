b64cdef8eb293d6d0620a1c013b9541eccb5ac54
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-12-17 18:37:33 +0900

    Upgrade to Java 21 API

--- nuxeo-csv-importer-extended-core/src/test/java/org/nuxeo/labs/TestCSVImporterExtendedImpl.java
+++ nuxeo-csv-importer-extended-core/src/test/java/org/nuxeo/labs/TestCSVImporterExtendedImpl.java
@@ -22,7 +22,7 @@ import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.TransactionalFeature;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 import java.io.File;
 import java.util.List;
 
